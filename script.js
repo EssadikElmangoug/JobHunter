a976130ef9ad3830d41384d2b3d4ed0b792e25db
EssadikElmangoug <essadik.designs@gmail.com>
2023-12-16 11:42:03 +0100

    mobile responsive

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMenu(){
+    const menu = document.querySelector(".sidebar");
+    menu.style.display = "flex"
+}
+
+function closeMenu(){
+    const menu = document.querySelector(".sidebar");
+    menu.style.display = "none"
+}
